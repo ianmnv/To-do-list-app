6fef732c15a85f5b2f2a1897d4025c9f044df29f
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-09-20 22:42:57 -0600

    Morgan middleware added to log requests

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 const app = express();
 
 const todosRouter = require("./router/todos.router");
 
-app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
 );
+
+app.use(morgan("common"));
+
+app.use(express.json());
 
 app.use("/todos", todosRouter);
 
