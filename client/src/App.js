68575c4ea3619a0b398673be1c4db72afa1f4df8
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-09-20 20:27:58 -0600

    POST request created for the front-end, with validations & Context file added

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,83 @@
-import "./App.css";
+import { useEffect, useState } from "react";
 
+import { useImmer } from "use-immer";
+
+import "./App.css";
 import ToDos from "./components/To-dos";
 
+import FullContext from "./FullContext";
+
 function App() {
+  const API_URL = "http://localhost:9000";
+  const [todoData, setTodoData] = useState([]);
+
+  const [state, dispatch] = useImmer({
+    id: 0,
+    toDo: "",
+    completed: false,
+  });
+
+  async function httpGetToDos() {
+    const response = await fetch(`${API_URL}/todos`);
+    const todos = await response.json();
+    setTodoData(todos);
+
+    dispatch((draft) => {
+      draft.id = todoData.length;
+    });
+  }
+
+  useEffect(() => {
+    httpGetToDos();
+  }, []);
+
+  async function submitCallback(e) {
+    e.preventDefault();
+
+    if (!state.toDo) {
+      return alert("Please fill the to-do input");
+    }
+
+    try {
+      return await fetch(`${API_URL}/todos`, {
+        method: "post",
+        body: JSON.stringify(state),
+        headers: {
+          "Content-type": /json/,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      return { error: error };
+    }
+  }
+
   return (
-    <main>
-      <header className="align">
-        <h1>To-do list mini app</h1>
-      </header>
+    <FullContext.Provider value={{ httpGetToDos, todoData }}>
+      <main>
+        <header className="align">
+          <h1>To-do list mini app</h1>
+        </header>
 
-      <div className="main-container align">
-        <form action="submit" id="save-new-todo">
-          <input type="text" name="input-to-do" id="input-to-do" />
-          <button id="btn-save">Save</button>
-        </form>
+        <div className="main-container align">
+          <form action="submit" id="save-new-todo" onSubmit={submitCallback}>
+            <input
+              type="text"
+              name="input-to-do"
+              id="input-to-do"
+              onChange={(e) =>
+                dispatch((draft) => {
+                  draft.toDo = e.target.value;
+                })
+              }
+            />
+            <button id="btn-save">Save</button>
+          </form>
 
-        <ToDos />
-      </div>
-    </main>
+          <ToDos />
+        </div>
+      </main>
+    </FullContext.Provider>
   );
 }
 
